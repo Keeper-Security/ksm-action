8f9977c47c68a45ac7655dfb5d4fafa80bcda11c
Maksim Ustinov <maksimu@gmail.com>
2021-11-03 15:37:11 -0700

    Fix splitting input that has spaces in the label names Added unit tests for splitting

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -11,3 +11,20 @@ test('Record uid extraction Ok', () => {
     const recordUids = getRecordUids(parseSecretsInputs(['BediNKCMG21ztm5xGYgNww/field/login > username', 'BediNKCMG21ztm5xGYgNww/field/password > password']))
     expect(recordUids).toStrictEqual(['BediNKCMG21ztm5xGYgNww'])
 })
+
+test('Input and Destination Splitting Ok', () => {
+    const parsedInputs = parseSecretsInputs([
+        'BediNKCMG21ztm5xGYgNww/field/a b>ab',
+        'BediNKCMG21ztm5xGYgNww/field/a b >ab',
+        'BediNKCMG21ztm5xGYgNww/field/a b > ab',
+        'BediNKCMG21ztm5xGYgNww/field/a b   >ab',
+        'BediNKCMG21ztm5xGYgNww/field/a b>   ab',
+        'BediNKCMG21ztm5xGYgNww/field/a b   >   ab'
+    ])
+
+    parsedInputs.forEach((parsedInput, index) => {
+        expect(parsedInput.notation).toBe('BediNKCMG21ztm5xGYgNww/field/a b')
+        expect(parsedInput.destination).toBe('ab')
+        expect(parsedInput.destinationType).toBe(0)
+    })
+})
